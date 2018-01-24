4fde4a7be115bd266f7b2373310836111d71891d
J. Moilanen <jnsmoilanen@gmail.com>
2018-01-24 22:12:52 +0200

    1.13-1.14

--- 1/anectodes/src/index.js
+++ 1/anectodes/src/index.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: new Array(anecdotes.length).fill(0)
     }
   }
 
@@ -15,20 +16,57 @@ class App extends React.Component {
     }
   }
 
+  voteAnectode = () => {
+    const copyOfVotes = this.state.votes.slice()
+    copyOfVotes[this.state.selected]++
+    return () => {
+      this.setState({votes: copyOfVotes})
+    }
+  }
+
+  bestAnectode = () => {
+    const votes = this.state.votes.slice()
+    let best = 0
+
+    votes.forEach((vote, i) => {
+      if(vote > votes[best]) {
+        best = i
+      }
+    })
+
+    return best
+  }
+
   render() {
     return (
       <div>
         <h4>{this.props.anecdotes[this.state.selected]}</h4>
-        <Button handleClick={this.randomAnectode()} />
+        <p>has {this.state.votes[this.state.selected]} votes</p>
+        <Button text="Give me moooar!" handleClick={this.randomAnectode()} />
+        <Button text='vote' handleClick={this.voteAnectode()} />
+        <BestAnectode 
+          anectode={this.props.anecdotes[this.bestAnectode()]}
+          votes={this.state.votes[this.bestAnectode()]}
+        />
       </div>
     )
   }
 }
 
-const Button = ({handleClick}) => {
+const BestAnectode = ({anectode, votes}) => {
+  return (
+    <div>
+      <h4>anectode with most votes:</h4>
+      <p>{anectode}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const Button = ({text, handleClick}) => {
   return (
     <button onClick={handleClick}>
-      Give me moooar!
+      {text}
     </button>
   )
 }
