3474f09286b062e02fb416998e291bdf8b437d5d
J. Moilanen <jnsmoilanen@gmail.com>
2018-01-28 19:45:05 +0200

    2.6

---
+++ 2/puhelinluettelo/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: this.state.newName,
+      id: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(nameObject)
+
+    this.setState({
+      persons,
+      newName: ''
+    })
+
+  }
+
+  handleNameChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addName}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange}/>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <ul>
+          {this.state.persons.map(person => <li>{person.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
